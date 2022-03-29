ca7497023a7bb46d2169ee77305d5f10251e5cef
Hector Maddock-Greene <rope.hmg@protonmail.com>
2022-03-29 18:10:22 +0100

    Updated to latest dprint version (#5)

--- dprint.py
+++ dprint.py
@@ -5,12 +5,12 @@ import subprocess
 import json
 import re
 
-dprint_exec = DprintExec()
 cached_dir_results = {}
 
 class DprintFmtCommand(sublime_plugin.TextCommand):
 	def description(self):
 		return "Formats source code using dprint."
+
 	def run(self, edit):
 		try:
 			file_path = self.view.file_name()
@@ -27,20 +27,22 @@ class DprintFmtCommand(sublime_plugin.TextCommand):
 				file_region = sublime.Region(0, self.view.size())
 				file_text = self.view.substr(file_region)
 				formatted_text = dprint_exec.format_text(dir_path, file_path, file_text)
+
 				if file_text != formatted_text:
 					self.view.replace(edit, file_region, formatted_text)
 					print("dprint: Formatted " + file_path)
+
 		except Exception as err:
 			print("dprint: " + str(err))
-		
+
 class EventListener(sublime_plugin.EventListener):
 	def on_pre_save(self, view):
 		view.run_command("dprint_fmt")
 
 class DprintExec:
 	def get_plugin_infos(self, dir_path):
-		expected_schema_version = 1
-		json_text = subprocess.check_output(["dprint", "editor-info"], cwd=dir_path, shell=True).decode("utf8")
+		expected_schema_version = 4
+		json_text = subprocess.check_output(["dprint", "editor-info"], cwd=dir_path).decode("utf8")
 		editor_info = json.loads(json_text)
 
 		if editor_info["schemaVersion"] != expected_schema_version:
@@ -50,18 +52,18 @@ class DprintExec:
 				raise Exception("Your installed version of dprint is out of date. Please update it.")
 
 		return editor_info["plugins"]
+
 	def format_text(self, dir_path, file_path, file_text):
 		from subprocess import Popen, PIPE
-		p = Popen(["dprint", "stdin-fmt", "--file-name", os.path.basename(file_path)],
-			stdin=PIPE, stdout=PIPE, stderr=PIPE, cwd=dir_path, shell=True)
+
+		p = Popen(["dprint", "fmt", "--stdin", os.path.basename(file_path)],
+			stdin=PIPE, stdout=PIPE, stderr=PIPE, cwd=dir_path)
 		stdout, stderr = p.communicate(bytes(file_text, "utf8"))
+
 		if p.returncode == 0:
-			result = stdout.decode("utf8")
-			if result.endswith("\r\n"):
-				return result[0:-2]
-			elif result.endswith("\n"):
-				return result[0: -1]
-			else:
-				return result
+			return stdout.decode("utf8")
 		else:
 			raise Exception("Error formatting: " + stderr.decode("utf8"))
+
+dprint_exec = DprintExec()
+
